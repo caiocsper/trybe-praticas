5b786593045b1ee238c7244ae0ef7951edd95ac6
caiocsper <ccpereira0498@gmail.com>
2022-07-21 03:37:58 -0300

    Praticando react, introdução a react - bloco10/dia01

---
+++ front-end/bloco-10_introducao-a-react/dia-1_hello-world-no-react/Ex_para-fixar/my-first-component/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './About';
+import './App.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+            <div>
+              <About/>
+            </div>
+           );
+  }
+}
+
+export default App;
