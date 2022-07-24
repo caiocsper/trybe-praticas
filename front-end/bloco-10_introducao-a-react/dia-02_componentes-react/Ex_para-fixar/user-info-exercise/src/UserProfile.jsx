8dc5bacbc71978f9a24652c512254c7f74aa8861
caiocsper <ccpereira0498@gmail.com>
2022-07-24 19:05:12 -0300

    Exercicio de fixação[Users Info] - exibindo informações dinamicamente com react e mesclando multiplos componentes

---
+++ front-end/bloco-10_introducao-a-react/dia-02_componentes-react/Ex_para-fixar/user-info-exercise/src/UserProfile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+    const { name, email, avatar } = this.props.user;
+
+    return (
+      <div>
+        <p> {name} </p>
+        <p> {email} </p>
+        <Image
+          source          = { avatar }
+          alternativeText = "User avatar"
+        />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
